9d4108785a9d072d9b04daa6721c8e38176ddacd
Багаутдинов-Ринат <tensal@bk.ru>
2025-08-05 22:10:36 +0300

    hw_27: Настройка сигнала

--- barbershop_project/core/signals.py
+++ barbershop_project/core/signals.py
@@ -1,18 +1,24 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from .models import Review
-from .mistral import is_bad_review
+from core.models import Order
+from core.telegram import send_telegram_message
+import logging
 
-@receiver(post_save, sender=Review)
-def check_review(sender, instance, created, **kwargs):
-    if created:
-        instance.ai_checked_status = "ai_checked_in_progress"
-        instance.save()
+logger = logging.getLogger(__name__)
 
-        if is_bad_review(instance.text):
-            instance.ai_checked_status = "ai_cancelled"
-            instance.is_published = False
-        else:
-            instance.ai_checked_status = "ai_checked_true"
-            instance.is_published = True
-        instance.save()
+@receiver(m2m_changed, sender=Order.services.through)
+def notify_about_new_order(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        try:
+            message = (
+                f"🎉 *Новый заказ!*\n"
+                f"🔹 *Клиент:* {instance.client_name}\n"
+                f"📞 *Телефон:* {instance.phone}\n"
+                f"🧔 *Мастер:* {instance.master.name if instance.master else 'Не указан'}\n"
+                f"📅 *Дата записи:* {instance.appointment_date}\n"
+                f"🔧 *Услуги:* {', '.join(s.name for s in instance.services.all())}"
+            )
+            if not send_telegram_message(message):
+                logger.error("Не удалось отправить сообщение в Telegram")
+        except Exception as e:
+            logger.error(f"Ошибка в сигнале: {e}")
